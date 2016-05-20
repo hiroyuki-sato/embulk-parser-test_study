5ea3a8da938bc5b7c1b8d286cfc7ac1712976af9
Hiroyuki Sato <hiroysato@gmail.com>
2016-05-20 16:38:19 +0900

    Implement plugin

--- src/main/java/org/embulk/parser/test_study/TestStudyParserPlugin.java
+++ src/main/java/org/embulk/parser/test_study/TestStudyParserPlugin.java
@@ -7,35 +7,36 @@ import org.embulk.config.ConfigDiff;
 import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
 import org.embulk.config.TaskSource;
+import org.embulk.spi.ColumnConfig;
+import org.embulk.spi.Exec;
+import org.embulk.spi.PageBuilder;
 import org.embulk.spi.ParserPlugin;
 import org.embulk.spi.FileInput;
 import org.embulk.spi.PageOutput;
 import org.embulk.spi.Schema;
 import org.embulk.spi.SchemaConfig;
+import org.embulk.spi.time.TimestampParser;
+import org.embulk.spi.util.LineDecoder;
+
+import java.util.ArrayList;
+
+import static org.embulk.spi.type.Types.LONG;
+import static org.embulk.spi.type.Types.STRING;
+import static org.embulk.spi.type.Types.TIMESTAMP;
+
 
 public class TestStudyParserPlugin
         implements ParserPlugin
 {
     public interface PluginTask
-            extends Task
+            extends Task,LineDecoder.DecoderTask, TimestampParser.Task
     {
         // configuration option 1 (required integer)
-        @Config("option1")
-        public int getOption1();
+        // @Config("option1")
+        // public int getOption1();
 
-        // configuration option 2 (optional string, null is not allowed)
-        @Config("option2")
-        @ConfigDefault("\"myvalue\"")
-        public String getOption2();
 
-        // configuration option 3 (optional string, null is allowed)
-        @Config("option3")
-        @ConfigDefault("null")
-        public Optional<String> getOption3();
 
-        // if you get schema from config or data source
-        @Config("columns")
-        public SchemaConfig getColumns();
     }
 
     @Override
@@ -43,18 +44,47 @@ public class TestStudyParserPlugin
     {
         PluginTask task = config.loadConfig(PluginTask.class);
 
-        Schema schema = task.getColumns().toSchema();
+        ArrayList<ColumnConfig> columns = new ArrayList<ColumnConfig>();
+
+        columns.add(new ColumnConfig("col1",   STRING, config));
+        columns.add(new ColumnConfig("col2",   STRING, config));
+        columns.add(new ColumnConfig("col3",   LONG, config));
+        
+        Schema schema = new SchemaConfig(columns).toSchema();
 
         control.run(task.dump(), schema);
     }
 
     @Override
     public void run(TaskSource taskSource, Schema schema,
-            FileInput input, PageOutput output)
-    {
+            FileInput input, PageOutput output) {
         PluginTask task = taskSource.loadTask(PluginTask.class);
+        LineDecoder lineDecoder = new LineDecoder(input, task);
+        PageBuilder pageBuilder = new PageBuilder(Exec.getBufferAllocator(), schema, output);
 
-        // Write your code here :)
-        throw new UnsupportedOperationException("TestStudyParserPlugin.run method is not implemented yet");
+        String line = null;
+        String[] elements;
+
+        while (input.nextFile()) {
+            while (true) {
+                line = lineDecoder.poll();
+                if (line == null) {
+                    break;
+                }
+                elements = line.split("\t", -1);
+                System.out.println(elements.length);
+
+                if( elements.length <  3 ) {
+                    continue;
+                }
+
+                pageBuilder.setString(0, elements[0]);
+                pageBuilder.setString(1, elements[1]);
+                pageBuilder.setLong(2, Integer.parseInt(elements[2]));
+                pageBuilder.addRecord();
+
+            }
+        }
+        pageBuilder.finish();
     }
 }
